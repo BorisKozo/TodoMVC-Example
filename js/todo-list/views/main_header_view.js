4c26671e25d29e4a510c11b6b7480e8ec8634cb1
Elad Moshe <elad.moshe@hp.com>
2013-02-04 17:25:27 +0200

    implemented basic tests

--- js/todo-list/views/main_header_view.js
+++ js/todo-list/views/main_header_view.js
@@ -1,28 +1,37 @@
 ﻿/*global define*/
 
 
-define(['marionette', 'hbs!./templates/main_header', './../controller'], function (Marionette, headerTemplate, controller) {
-    'use strict';
-    var MainHeaderView = Marionette.ItemView.extend({
-        template: headerTemplate,
-        ui: {
-            'input': '#new-todo'
-        },
-        events: {
-            'keypress #new-todo': 'inputKeypress'
-        },
+define(['marionette', 'hbs!./templates/main_header', './../controller'], function(Marionette, headerTemplate, controller) {
+   'use strict';
+   var MainHeaderView = Marionette.ItemView.extend({
+      template: headerTemplate,
+      ui: {
+         'input': '#new-todo'
+      },
+      events: {
+         'keypress #new-todo': 'inputKeypress',
+         'click #submit-new-todo': 'simulateEnterKeypress'
+      },
 
-        inputKeypress: function (e) {
-            var ENTER_KEY = 13,
-             todoText = this.ui.input.val().trim();
+      inputKeypress: function(e) {
+         var ENTER_KEY = 13,
+            todoText = this.ui.input.val().trim();
 
-            if (e.which === ENTER_KEY && todoText) {
-                controller.vent.trigger('todoTextReady', todoText);
-                this.ui.input.val('');
-            }
-        }
+         if (e.which === ENTER_KEY && todoText) {
+            controller.vent.trigger('todoTextReady', todoText);
+            this.ui.input.val('');
+         }
+      },
 
-    });
+      /*
+      This function is only used for testing. We added a transparent button on which the
+      testing framework clicks to add the item
+       */
+      simulateEnterKeypress: function(){
+         this.inputKeypress({which: 13});
+      }
 
-    return MainHeaderView;
+   });
+
+   return MainHeaderView;
 });
--- test/spec.js
+++ test/spec.js
@@ -2,46 +2,108 @@
 process.env.NODE_ENV = 'test';
 var Browser = require('zombie');
 var expect = require('chai').expect;
+var baseUrl = "localhost:1234";
+var secondaryUrl = '/index.html#';
 
 describe("Todo Application", function() {
-   var browser = new Browser({
-//      debug: true,
-      proxy: "http://web-proxy.isr.hp.com:8080",
-      site: "http://addyosmani.github.com/todomvc/architecture-examples/backbone/",
-      waitFor: 2
-   });
+   var that = this;
 
-   function addItem(text, done) {
-      browser.fill("#new-todo", "text");
-
-      console.log(browser.fill("#new-todo"));
-      done();
+   function addItem(text, callback) {
+      that.browser.fill("#new-todo", text);
+      that.browser.pressButton("#submit-new-todo");
+      callback();
    }
 
-   before(function(done) {
-      browser.visit('#/', done);
+   function waitForPageLoadEnd(callback) {
+      that.browser.wait(function() {
+         return that.browser.querySelector("#new-todo");
+      }, callback);
+   }
+
+   beforeEach(function(callback) {
+      that.browser = new Browser({
+         //debug: true,
+         //proxy: "http://web-proxy.isr.hp.com:8080",
+         site: baseUrl,
+         waitFor: 4
+      });
+      that.browser.visit(secondaryUrl, null, function() {
+         waitForPageLoadEnd(callback);
+      });
    });
 
 
-   it('shows "clear completed" button when there are completed items in the list', function(done) {
-      addItem("first item", function () {
-         expect(browser.querySelector("#todo-count")).to.exist;
+   it('shows how many items left', function() {
+      expect(that.browser.text("#todo-count strong")).to.equal("0");
+      addItem("first item", function() {
+         expect(that.browser.text("#todo-count strong")).to.equal("1");
+         addItem("second item", function() {
+            expect(that.browser.text("#todo-count strong")).to.equal("2");
+         });
+      });
+   });
+
+
+//   it("saves the state after page refresh", function(done) {
+//      addItem("first item", function() {
+//         expect(that.browser.text("#todo-count strong")).to.equal("1");
+//      });
+//      that.browser.reload(function() {
+//         waitForPageLoadEnd(function() {
+//         console.log(that.browser.html());
+////            expect(that.browser.text("#todo-count strong")).to.equal("1");
+//            done();
+//         })
+//      })
+//   });
+
+   it("can mark an item as completed", function(done){
+      expect(that.browser.querySelector(".completed")).not.to.exist;
+      addItem("first item",function(){
+         that.browser.check(".toggle");
+         expect(that.browser.querySelector(".completed")).to.exist;
+         done();
+      })
+   });
+
+   it("allows marking all items as completed using the toggle all button", function(done){
+      expect(that.browser.querySelector(".completed")).not.to.exist;
+      addItem("first item",function(){
+         addItem("second item",function(){
+            that.browser.check("#toggle-all");
+            expect(that.browser.querySelectorAll(".completed").length).to.eql(2);
+            done();
+         });
+      });
+   });
+
+   it("doesn't allow to add en empty item",function(done){
+      expect(that.browser.querySelectorAll("li.active").length).to.eql(0);
+      addItem("",function(){
+         expect(that.browser.querySelectorAll("li.active").length).to.eql(0); //after adding empty item, no new item was created
          done();
       });
+   });
+
+   xit("allows to filter only active items", function(){
 
    });
 
-   //   it("allows to add a new todo item", function() {
+   /*xit('shows "clear completed" button when there are completed items in the list', function(done) {
+
+    });*/
+
+//   it("allows to add a new todo item", function() {
 //
 //
 //      assert(browser.querySelector("#header"))
 //      console.log("myheader it", browser.querySelector("#header"));
 //   });
-   /*it("saves the state after page refresh");
-    it("can mark an item as completed");
+   /*
+
     it("allows deleting an item");
     it("allows editing an item using mouse double click");
-    it("allows marking all items as completed using the toggle all button");
+
     it("shows how many items are left to be done");
     it('has a "todo" header');
     it("shows all items by default");
@@ -53,6 +115,10 @@ describe("Todo Application", function() {
    /*
     it('does not show "clear completed" button where there are no completed items in the list');
     it("shows the destroy icon only when hovering over an item");
-    it('shows a counter of completed items');*/
+    */
 
-});
+
+})
+;
+
+
